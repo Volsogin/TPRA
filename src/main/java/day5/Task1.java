f285dadda28553e0e6d766b16db013a953d77ea4
Volsogin <vologin2010@gmail.com>
2024-05-20 18:52:38 +0400

    add 5 day

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,56 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car huananzhi = new Car();
+        huananzhi.setModel("Huananzhi Type ZeroY");
+        huananzhi.setData(2024);
+        huananzhi.setColor("Белый");
+        System.out.println("Всем привет, братья. Взял себе новый "+huananzhi.getColor() +
+                " " + huananzhi.getModel() + ". Взял последнюю модель " + huananzhi.getData() +" года выпуска" );
+        System.out.println(huananzhi.getColor() + " " + huananzhi.getModel() + " " + huananzhi.getData());
+
+
+
+
+
+
+
+
 
     }
 }
+class Car {
+    private String model;
+    private String color;
+    private int data;
+    public void setModel(String AutoModel) {
+     model = AutoModel;
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public void setColor(String AutoColor) {
+        color = AutoColor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setData(int AutoData) {
+        data = AutoData;
+    }
+    public int getData() {
+        return data;
+    }
+
+
+
+
+
+}
+
+
+
+
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,59 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike voge = new Motorbike("Voge 300R", "Чёрный", 2024);
+        voge.getInfo();
+
+
+
+
+
+
 
     }
 }
+
+class Motorbike {
+    private String model;
+    private String color;
+    private int date;
+
+    public Motorbike(String model, String color, int date) {
+        this.model = model;
+        this.color = color;
+        this.date = date;
+    }
+
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getDate(){
+        return date;
+    }
+    public void getInfo(){
+        System.out.println(getColor() + " " + getModel() + " " + getDate() + " года выпуска");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
